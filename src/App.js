a30644bbeebbb8d5a374b0e20d8918be2227fd92
Anderson Ventura Cerqueira <82953125+VenturaCerqueira@users.noreply.github.com>
2025-02-25 11:13:28 -0300

    save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/Navbar';
+import LandingPage from './LandingPage'; 
+import Card from './components/Card'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LandingPage />
+      <Navbar />
+      <Card /> 
+      
     </div>
   );
 }
